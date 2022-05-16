591fa559f2875e3445198610a98cec79f38a4064
Gleepoj <63968154+Gleepoj@users.noreply.github.com>
2022-05-16 18:17:15 +0200

    ++ Minor cleaning VHC func

--- src/game/aleiiioa/components/vehicule/VehiculeComponent.hx
+++ src/game/aleiiioa/components/vehicule/VehiculeComponent.hx
@@ -5,7 +5,7 @@ import h3d.Vector;
 class VehiculeComponent {
     public var maxSpeed = 0.8;//0.8
     public var maxForce = 0.5;//0.05
-    public var friction = 0.993;
+    public var friction = 0.99;
     
     public var mass = 1.6;// * Math.random(2);//Math.random(3);
 
@@ -21,12 +21,15 @@ class VehiculeComponent {
     public var acceleration:Vector;
     public var euler:Vector;
 
-    public var desiredAngle (get,never):Float; inline function get_desiredAngle()  return desired.getPolar();
+    
     public var steeringAngle(get,never):Float; inline function get_steeringAngle() return steering.getPolar();
     public var vehiculeAngle(get,never):Float; inline function get_vehiculeAngle() return orientation.getPolar();
     
     public var speed (get,never):Float; inline function get_speed() return velocity.length();
+    
     public var origin(get,never):Vector; inline function get_origin() return new Vector(0,-1,0,0);
+    public var zero(get,never):Vector; inline function get_zero() return new Vector();
+
     public var board(get,never):Vector; inline function get_board() return new Vector(Math.cos(vehiculeAngle + Math.PI/2),Math.sin(vehiculeAngle + Math.PI/2));
     public var predicted(get,never):Vector; inline function get_predicted() return VectorUtils.predict(location,velocity);
     
@@ -52,16 +55,13 @@ class VehiculeComponent {
     }
 
     public function addTorque(a:Float){
-        var or = steeringAngle;
-        var a = new Vector(Math.cos(or+a)*1,Math.sin(or+a)*1);
-        steering = a.normalized();
+        var o = steeringAngle;
+        steering =  new Vector(Math.cos(o+a)*1,Math.sin(o+a)*1).normalized();
     }
 
     public function accelerationFriction(){
-        var zero = new Vector();
-        var frict = new Vector();
-        var a = acceleration.clone();
-        frict.lerp(zero,a,friction);
-        acceleration = frict;
+        var af = new Vector();
+        af.lerp(zero,acceleration,friction);
+        acceleration = af;
     }    
 }
